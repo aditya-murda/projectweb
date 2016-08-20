abac96f50747f845c7719cb4a056322e0d1dd082
Aditya Murda Nugraha <aditya_murda@yahoo.co.id>
2016-08-20 16:38:35 +0900

    database dibenerin, bikin rest post

--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,35 @@ var mysql = require("mysql");
 var pool = mysql.createPool({
     connectionLimit: 3,
     host: 'localhost',
-    user: 'minkkang',
-    database: 'test',
-    password: '161718'
+    user: 'root',
+    password: 'root',
+    database: 'test'
 });
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Undercover' });
+  pool.getConnection(function (err, connection) {
+      if(connection) {
+          console.log("connection");
+      }
+      // TODO: debug
+      connection.query("SELECT * FROM users", function (err, rows) {
+          if(err) console.error("err db: ", err);
+          console.log("rows: " + JSON.stringify(rows));
+          res.render('index', {title: 'View Table', rows: rows});
+          connection.release();
+      });
+  });
+});
+
+router.get("/post", function (req, res, next) {
+    var username = req.param("username");
+    var password = req.param("password");
+    var post = req.param("post")
+    pool.getConnection(function (err, connection) {
+        connection.query("");
+    });
 });
 
 module.exports = router;
