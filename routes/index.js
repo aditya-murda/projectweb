941b64569f8dde42d15a41cb182414878512abfe
Cita Audia Ahlunaza <cita.audia.ahlunaza@gmail.com>
2016-08-20 14:00:48 +0900

    ganti routing ke views/index.ejs

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send("здравствуйте");
+  res.render('index', { title: 'Undercover' });
 });
 
 module.exports = router;
