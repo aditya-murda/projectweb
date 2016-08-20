c8141f922595054d09b31c8dee49fd71f376e090
Aditya Murda Nugraha <aditya_murda@yahoo.co.id>
2016-08-20 15:04:31 +0900

    push server2

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require("mysql");
+var pool = mysql.createPool({
+    connectionLimit: 3,
+    host: 'localhost',
+    user: 'minkkang',
+    database: 'test',
+    password: '161718'
+});
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
